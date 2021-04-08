69b598898a18cdce63208d223a412ba5050b5207
Voraque <55339770+Voraque@users.noreply.github.com>
2021-04-07 17:18:43 -0700

    Updated city spawner script; added UI; cleaned up file structure
    Updated City Spawner game object to allow for an array of objects. Script will randomly select from the array to create a city with size X by X. City is generated from UI, and city size can be selected at that time. Removed .mtl files, since Unity doesn't use them.

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -1,17 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ObjectSpawner : MonoBehaviour
 {
-    [SerializeField] Transform cube;
-    [SerializeField] int numberOfCubes;
+    [SerializeField] Transform[] buildings;
+    [SerializeField] Vector3 buildingSize = Vector3.one;
+    float buildingScaleFactor = 0.5f;
+    
+    Transform buildingInstance;
+    Vector2 offset = new Vector2(0.5f, 0.5f);
+    int citySize;
 
+    public void updateCitySize(string citySizeString) {
+        citySize = Convert.ToInt32(citySizeString);
+    }
 
-    private void Start() {
-        for (int i = 0; i < numberOfCubes; i++) {
-            Transform cubeInstance = Instantiate(cube);
-            cubeInstance.localPosition = Vector3.right * i;
+    public void GenerateCity() {
+        for (int x = 0; x < citySize; x++) {
+            for (int y = 0; y < citySize; y++) {
+                buildingInstance = Instantiate(
+                    buildings[UnityEngine.Random.Range(0, buildings.Length)]);
+                buildingInstance.localPosition = new Vector3(x + offset.x, 0f, y + offset.y);
+                buildingInstance.localScale = buildingSize * buildingScaleFactor;
+            }
         }
     }
 }
